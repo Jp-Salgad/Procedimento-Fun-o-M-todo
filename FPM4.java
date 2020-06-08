6003dace372141a070d014f93116b7e62f444b08
Jp-Salgad <63624496+Jp-Salgad@users.noreply.github.com>
2020-06-08 11:55:59 -0300

    Update FPM4.java

--- FPM4.java
+++ FPM4.java
@@ -9,37 +9,37 @@ public class FPM4 {
         double a, b;
         int ab;
         
-        System.out.println("Por favor, escreva dois números.");
+        System.out.println("Por favor, escreva dois nÃºmeros.");
         System.out.println("Por favor, escreva o primeiro:");
 		a=leitor.nextDouble();
 
         System.out.println("Por favor, escreva o segundo:");
 		b=leitor.nextDouble();
 
-        System.out.println("Por favor, agora escolha o tipo de operação:");
+        System.out.println("Por favor, agora escolha o tipo de operaÃ§Ã£o:");
         
         System.out.println("1- Soma");
-		System.out.println("2- Subtração");
-		System.out.println("3- Divisão");
-		System.out.println("4- Multiplicação");
+		System.out.println("2- SubtraÃ§Ã£o");
+		System.out.println("3- DivisÃ£o");
+		System.out.println("4- MultiplicaÃ§Ã£o");
 		ab=leitor.nextInt();
 		
 		switch (ab) {
 		
 		case 1:
-			System.out.println("O resultado é: "+(a+b));
+			System.out.println("O resultado Ã©: "+(a+b));
 			break;
 		case 2:
-			System.out.println("O resultado é: "+(a-b));
+			System.out.println("O resultado Ã©: "+(a-b));
 			break;
 		case 3 :
-			System.out.println("O resultado é: "+(a/b));
+			System.out.println("O resultado Ã©: "+(a/b));
 			break;
 		case 4 :	
-			System.out.println("O resultado é: "+(a*b));
+			System.out.println("O resultado Ã©: "+(a*b));
 			break;
 			
-			default: System.out.println("Não é possível completar essa operação.");
+			default: System.out.println("ERRO! NÃ£o foi possÃ­vel realizar a operaÃ§Ã£o. Tente novamente.");
 		
 		
 		
